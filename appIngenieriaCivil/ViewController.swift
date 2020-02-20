2fb6ca91268561f0b02e4bff021b2007166c9a9f
tavotd <tavo_decadente14@hotmail.com>
2020-02-20 13:51:52 -0600

    archivos base del proyecto

---
+++ appIngenieriaCivil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appIngenieriaCivil
+//
+//  Created by CEDAM05 on 20/02/20.
+//  Copyright © 2020 CEDAM05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
